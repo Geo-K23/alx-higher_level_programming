021aab9561620f6c81daaa46a0e952d58a0bb0cb
George Kazibwe <gkazibwe@gmail.com>
2023-07-03 08:13:31 -0700

    task 8 and 9

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,5 +5,5 @@ This script contains the "class_to_json" function
 
 
 def class_to_json(obj):
-    """Returns the dictionary description with simple data structure for JSON serialization of an object"""
+    """Returns the dictionary description with simple data structure"""
     return obj.__dict__
